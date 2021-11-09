5719d3661a319dc196c802f74103a7668de6a48d
Lateef Azeez <codelate18@gmail.com>
2021-11-09 01:27:15 +0000

    implement 4 and 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@all_stores = Store.all
+
+puts @all_stores.sum(:annual_revenue)
+puts @all_stores.average(:annual_revenue)
+
+@stores_making_one_million_or_more = Store.where('annual_revenue >= ?', 1000000).size()
+
+puts @stores_making_one_million_or_more
